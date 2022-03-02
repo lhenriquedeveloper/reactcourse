b9c64529c7caaeaf96d0cebd61271ca8ebd1217f
Luis Henrique Sousa <sousarodriguesluishenrique@gmail.com>
2022-03-02 15:58:40 -0300

    Renderização Condicional

--- react/meusite/src/App.js
+++ react/meusite/src/App.js
@@ -1,43 +1,21 @@
+// Renderização Condicional
+
 import React, { Component } from "react";
-
-//props = estáticas.
-//states = estados mutáveis.
-
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      contador: 0,
+      status: 1,
     };
-    this.aumentar = this.aumentar.bind(this);
-    this.diminuir = this.diminuir.bind(this);
-  }
-
-  aumentar() {
-    let state = this.state;
-    state.contador += 1;
-    this.setState(state);
-  }
-
-  diminuir() {
-    let state = this.state;
-    if (state.contador === 0) {
-      alert("O sistema não suporta valores negativos");
-    } else {
-      state.contador -= 1;
-      this.setState(state);
-    }
   }
 
   render() {
     return (
       <div>
-        <h2>CONTADOR:</h2>
-        <h3>
-          <button onClick={this.diminuir}>-</button>
-          {this.state.contador}
-          <button onClick={this.aumentar}>+</button>
-        </h3>
+        {this.state.status === 1 && <h1>Bem Vindo ao Sistema!</h1>}
+        <div>
+          <h2> React JS Course</h2>
+        </div>
       </div>
     );
   }
