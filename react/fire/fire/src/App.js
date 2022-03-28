b6136e97e235b73ee9f7125e8584379897e45bd2
Luis Henrique Sousa <sousarodriguesluishenrique@gmail.com>
2022-03-28 16:31:58 -0300

    Insert e Get no Cloud Firestore

--- react/fire/fire/src/App.js
+++ react/fire/fire/src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import firebase from "./firebaseConnection";
+import { useState } from "react";
+import "./style.css";
 
-function App() {
+export default function App() {
+  const [titulo, setTitulo] = useState("");
+  const [autor, setAutor] = useState("");
+
+  async function handleAdd() {
+    // Realizando inserção no firebase API com ID estático.
+    // await firebase
+    //   .firestore()
+    //   .collection("posts")
+    //   .doc("12345")
+    //   .set({
+    //     titulo: titulo,
+    //     autor: autor,
+    //   })
+    //   .then(() => {
+    //     console.log("Dados cadastrados com sucesso!");
+    //   })
+    //   .catch((error) => {
+    //     console.log("Algo deu errado: " + error);
+    //   });
+
+    //Realizando inserção no Firebase API com ID aleatório
+    await firebase
+      .firestore()
+      .collection("posts")
+      .add({
+        titulo: titulo,
+        autor: autor,
+      })
+      .then(() => {
+        console.log("Dados cadastrados com sucesso!");
+      })
+      .catch((error) => {
+        console.log("Algo deu errado: " + error);
+      });
+
+    setAutor("");
+    setTitulo("");
+  }
+
+  async function buscaPost() {
+    await firebase
+      .firestore()
+      .collection("posts")
+      .doc("123")
+      .get()
+      .then((snapshot) => {
+        setTitulo(snapshot.data().titulo);
+        setAutor(snapshot.data().autor);
+      })
+      .catch((error) => {
+        console.log("Algo deu errado: " + error);
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Trabalhando com Firebase</h1>
+      <div className="container">
+        <label>Titulo:</label>
+        <textarea
+          type="test"
+          value={titulo}
+          onChange={(e) => {
+            setTitulo(e.target.value);
+          }}
+        />
+        <label>Autor: </label>
+        <input
+          type="text"
+          value={autor}
+          onChange={(e) => {
+            setAutor(e.target.value);
+          }}
+        />
+        <button onClick={handleAdd}>Cadastrar</button>
+        <button onClick={buscaPost}>Buscar Post</button>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ react/fire/fire/src/firebaseConnection.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+let firebaseConfig = {
+  apiKey: "AIzaSyDFXrZHoSc2eO7nqCMenFV8kIfbSk_TD3M",
+  authDomain: "cursoreact-96949.firebaseapp.com",
+  projectId: "cursoreact-96949",
+  storageBucket: "cursoreact-96949.appspot.com",
+  messagingSenderId: "1021530283972",
+  appId: "1:1021530283972:web:a76e75c6f26bbc23f08e91",
+  measurementId: "G-Y3RJNTMM2Z",
+};
+
+// Abrindo conexão Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
