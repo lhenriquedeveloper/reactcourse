22d0e67b9bf43a3f9e3b94ea5f179d5d886ce74f
Luis Henrique Sousa <sousarodriguesluishenrique@gmail.com>
2022-04-01 16:32:09 -0300

    Login Firebase

--- react/fire/fire/src/App.js
+++ react/fire/fire/src/App.js
@@ -1,133 +1,12 @@
 import firebase from "./firebaseConnection";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./style.css";
 
 export default function App() {
-  const [idPost, setIdPost] = useState("");
-  const [titulo, setTitulo] = useState("");
-  const [autor, setAutor] = useState("");
-  const [posts, setPosts] = useState([]);
+  //STATES:
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
-  //Fazendo atualização das listas em RealTime com useEffect
-  useEffect(() => {
-    async function loadPosts() {
-      await firebase
-        .firestore()
-        .collection("posts")
-        .onSnapshot((doc) => {
-          let myPosts = [];
-
-          doc.forEach((item) => {
-            myPosts.push({
-              id: item.id,
-              titulo: item.data().titulo,
-              autor: item.data().autor,
-            });
-          });
-          setPosts(myPosts);
-        });
-    }
-    loadPosts();
-  }, []);
-
-  async function handleAdd() {
-    // Realizando inserção no firebase API com ID estático.
-    // await firebase
-    //   .firestore()
-    //   .collection("posts")
-    //   .doc("12345")
-    //   .set({
-    //     titulo: titulo,
-    //     autor: autor,
-    //   })
-    //   .then(() => {
-    //     console.log("Dados cadastrados com sucesso!");
-    //   })
-    //   .catch((error) => {
-    //     console.log("Algo deu errado: " + error);
-    //   });
-
-    //Realizando inserção no Firebase API com ID aleatório
-    await firebase
-      .firestore()
-      .collection("posts")
-      .add({
-        titulo: titulo,
-        autor: autor,
-      })
-      .then(() => {
-        console.log("Dados cadastrados com sucesso!");
-      })
-      .catch((error) => {
-        console.log("Algo deu errado: " + error);
-      });
-
-    setAutor("");
-    setTitulo("");
-  }
-
-  async function buscaPost() {
-    // Realizando GET de document por ID
-    // await firebase
-    //   .firestore()
-    //   .collection("posts")
-    //   .doc("123")
-    //   .get()
-    //   .then((snapshot) => {
-    //     setTitulo(snapshot.data().titulo);
-    //     setAutor(snapshot.data().autor);
-    //   })
-    //   .catch((error) => {
-    //     console.log("Algo deu errado: " + error);
-    //   });
-
-    //Realizando get de todos os posts
-    await firebase
-      .firestore()
-      .collection("posts")
-      .get()
-      .then((snapshot) => {
-        let lista = [];
-
-        snapshot.forEach((doc) => {
-          lista.push({
-            id: doc.id,
-            autor: doc.data().autor,
-            titulo: doc.data().titulo,
-          });
-        });
-        setPosts(lista);
-      })
-      .catch(() => {
-        console.log("Algo de errado não está certo");
-      });
-  }
-
-  async function editarPost() {
-    await firebase
-      .firestore()
-      .collection("posts")
-      .doc(idPost)
-      .update({
-        titulo: titulo,
-        autor: autor,
-      })
-      .then(() => {
-        setIdPost("");
-        setAutor("");
-        setTitulo("");
-        alert("Dados atualizados com sucesso");
-      })
-      .catch((error) => {
-        console.log("Algo deu errado, veja" + error);
-      });
-  }
-
-  async function excluirPost(id) {
-    await firebase.firestore().collection("posts").doc(id).delete();
-  }
 
   async function newUser() {
     await firebase
@@ -147,6 +26,11 @@ export default function App() {
       });
   }
 
+  async function logout() {
+    await firebase.auth().signOut();
+    console.log("Usuário deslogado");
+  }
+
   return (
     <div>
       <h1> Trabalhando com Firebase </h1>
@@ -164,6 +48,7 @@ export default function App() {
           }}
         />
         <br />
+
         <label>Senha: </label>
         <input
           type="password"
@@ -173,59 +58,10 @@ export default function App() {
             setPassword(e.target.value);
           }}
         />
-        <br />
-        <button onClick={newUser}>Cadastrar usuário</button> <br />
-      </div>
 
-      <h3>Post Manegement: </h3>
-      <div className="container">
-        <label>ID: </label>
-        <input
-          type="text"
-          onChange={(e) => {
-            setIdPost(e.target.value);
-          }}
-          value={idPost}
-        />
-        <label>Titulo:</label>
-        <textarea
-          type="test"
-          value={titulo}
-          onChange={(e) => {
-            setTitulo(e.target.value);
-          }}
-        />
-        <label>Autor: </label>
-        <input
-          type="text"
-          value={autor}
-          onChange={(e) => {
-            setAutor(e.target.value);
-          }}
-        />
-        <button onClick={handleAdd}>Inserir Post</button>
-        <button onClick={buscaPost}>Listar Posts</button>
-        <button onClick={editarPost}>Editar Post</button>
         <br />
-        <ul>
-          {posts.map((post) => {
-            return (
-              <li key={post.id}>
-                <span>ID: {post.id}</span>
-                <br />
-                <span>Titulo : {post.titulo}</span> <br />
-                <span>Autor: {post.autor}</span> <br />
-                <button
-                  onClick={() => {
-                    excluirPost(post.id);
-                  }}
-                >
-                  Excluir Post
-                </button>
-              </li>
-            );
-          })}
-        </ul>
+        <button onClick={newUser}>Cadastrar usuário</button>
+        <button onClick={logout}>Deslogar</button>
       </div>
     </div>
   );
